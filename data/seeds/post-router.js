60a23cb366c15d7a71b3ae85c65ac0f3ae86bbbb
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-29 19:08:27 +0300

    created post request for posts

--- data/seeds/post-router.js
+++ data/seeds/post-router.js
@@ -7,84 +7,84 @@ const router = express.Router();
 // router.use(cors());
 // router.use(express.json());
 
-router.post("/:id/comments",handlePostComments);
-router.get("/:id/comments",handleGetComments);
-router.get("/:id",handleGetPostByID);
-router.put("/:id",handlePostEdit);
-router.delete("/:id",handlePostDelete);
+router.post("/:id/comments", handlePostComments);
+router.get("/:id/comments", handleGetComments);
+router.get("/:id", handleGetPostByID);
+router.put("/:id", handlePostEdit);
+router.delete("/:id", handlePostDelete);
 router.post("/", handlePosts);
 router.get("/", handleAllGetPosts);
 
-function handlePostEdit(req,res) {
-    const {id} = req.params;
-    const post = {
-        title: req.body.title,
-        contents: req.body.contents
-      };
-    db.update(id,post)
-      .then(data => {
-          res.json(data);
-          console.log("response from router",data);
-      })
-      .catch(error => {
-          res.json({errorMessaga:error});
-          console.log("error from edit endpoint",error);
-      })      
+function handlePostEdit(req, res) {
+  const { id } = req.params;
+  const post = {
+    title: req.body.title,
+    contents: req.body.contents
+  };
+  db.update(id, post)
+    .then(data => {
+      res.json(data);
+      console.log("response from router", data);
+    })
+    .catch(error => {
+      res.json({ errorMessaga: error });
+      console.log("error from edit endpoint", error);
+    });
 }
 
-function handlePostDelete(req,res) {
-    const {id} = req.params;
-    db.remove(id)
-      .then(data => {
-          res.json(data);
-          console.log("removed",data);
-      })
-      .catch(error => {
-          res.json(error);
-          console.log("error from delete endpoint",error);
-      })  
+function handlePostDelete(req, res) {
+  const { id } = req.params;
+  db.remove(id)
+    .then(data => {
+      res.json(data);
+      console.log("removed", data);
+    })
+    .catch(error => {
+      res.json(error);
+      console.log("error from delete endpoint", error);
+    });
 }
 
-function handleGetPostByID(req,res) {
-    const {id} = req.params;    
-    db.findById(id)
-      .then(data => {
-          res.json(data);
-          console.log("response from get post by id",data);
-      }) 
-      .catch(error => {
-          res.json({errorMessaga:error});
-          console.log("error from get post by id",error);
-      }) 
+function handleGetPostByID(req, res) {
+  const { id } = req.params;
+  db.findById(id)
+    .then(data => {
+      res.json(data);
+      console.log("response from get post by id", data);
+    })
+    .catch(error => {
+      res.json({ errorMessaga: error });
+      console.log("error from get post by id", error);
+    });
 }
 
-function handleGetComments(req,res) {
-    const {id} = req.params;
-    db.findCommentById(id)
-      .then(data => {
-          res.json(data);
-          console.log("response from get comment by id",data);
-      }) 
-      .catch(error=> {
-          res.json({errorMessaga:error});
-          console.log("error from get comment by ID", error);
-      })
+function handleGetComments(req, res) {
+  const { id } = req.params;
+  db.findCommentById(id)
+    .then(data => {
+      res.json(data);
+      console.log("response from get comment by id", data);
+    })
+    .catch(error => {
+      res.json({ errorMessaga: error });
+      console.log("error from get comment by ID", error);
+    });
 }
 
-function handlePostComments(req,res) {
-    const comment = {
-        text: req.body.text,
-        post_id: req.params.id
-    }
-    db.insertComment(comment)
-      .then(data => {
-          res.json(data);
+function handlePostComments(req, res) {
+  const comment = {
+    text: req.body.text,
+    post_id: req.params.id
+  };
+  db.insertComment(comment)
+    .then(data => {
+      res.json(data);
       console.log("response from  post comments endpoint", data);
-      })
-      .catch(error => {
-          res.json({errorMessage:error});
+    })
+    .catch(error => {
+      res.json({ errorMessage: error });
       console.log("error from post comments endpoint", error);
-      })  
+    });
 }
 
 function handleAllGetPosts(req, res) {
@@ -110,8 +110,14 @@ function handlePosts(req, res) {
       console.log("response from posts endpoint", data);
     })
     .catch(error => {
-      res.json({ errorMessage: error });
-      console.log("error from posts endpoint", error);
+      if (post.title || post.contents === undefined) {
+        res.json({
+          errorMessage: "Please provide title and contents for the post."
+        });
+      } else if (post.title || post.contents !== undefined) {
+        res.json({ errorMessage: error });
+        console.log("error from posts endpoint", error);
+      }
     });
 }
 
